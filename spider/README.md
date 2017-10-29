552d6369dc506786c98395259d21767d4c702cb1
kingdom7594 <kingdom7594@gmail.com>
2017-10-29 21:35:56 +0800

    Spider 测试demo

---
+++ spider/README.md
@@ -0,0 +1,2 @@
+### Python 爬虫框架Scrapy
+学习用 [Scrapy](https://doc.scrapy.org/en/latest/intro/tutorial.html)爬取感兴趣网站的数据并保存到mysql数据库中，为后期用Java 搭建博客网站提供数据来源。
--- spider/reddit/spiders/redditor.py
+++ spider/reddit/spiders/redditor.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class RedditorSpider(scrapy.Spider):
     name = 'redditor'
-    allowed_domains = ['https://www.reddit.com/']
-    start_urls = ['http://https://www.reddit.com//']
+    allowed_domains = ['reddit.com/']
+    start_urls = ['https://www.reddit.com/']
 
     def parse(self, response):
-        pass
+        logging.warning("hello the world!")
